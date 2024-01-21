5f85877c3d958362d913a670189e634b426ceaa1
Shane Whitehead <obishiro@gmail.com>
2024-01-22 08:25:51 +1100

    Update the mobile snapshot

--- Poster-SwiftUI/Views/Posters/RemotePosterViewDelegate.swift
+++ Poster-SwiftUI/Views/Posters/RemotePosterViewDelegate.swift
@@ -17,6 +17,10 @@ struct RemotePosterViewDelegate: PosterViewDelegate {
     }
     
     func imageForPoster() async throws -> Image? {
+        // In a real world scenario, I'd consider having a image cache system
+        // in plce to first look up the image locally, if not found (or if the
+        // cache had expired the image), I'd touch the remote end to download
+        // and put the image in the cache
         let data = try await URLSession.shared.data(from: resourceUrl).0
         return Image(data: data)
     }
